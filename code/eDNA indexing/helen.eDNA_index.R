1a5354cff405ac4f36e36377b9554f4fd2631d47
hcasendino <hcas1024@uw.edu>
2021-08-13 15:52:53 -0700

    Convert NAs to 0s

--- code/eDNA indexing/helen.eDNA_index.R
+++ code/eDNA indexing/helen.eDNA_index.R
@@ -6,6 +6,7 @@
 # ===================================================
 # df: DATAFRAME with 1 column for each PCR run (data = nReads)
 # PCRcols: vector of PCR column indices (ie, 3:5)
+# Note: NAs come up when propReads/(max(propReads)) = 0/0, because some Hashes have no reads in certain PCRs 
 # ===================================================
 
 calc_eDNA_index <- function(df, PCRcols) { 
@@ -21,6 +22,9 @@ calc_eDNA_index <- function(df, PCRcols) {
       
       group_by(Hash) %>% # "scale the resulting proportion to the largest observed proportion across samples"
       mutate(index = propReads/(max(propReads)), .keep = "none") %>% ungroup() %>% select(-Hash)
+    
+    NArows <- which(is.na(df[,nRead.col]) == T) 
+    df[NArows,nRead.col] <- 0
   }
   colnames(df)[PCRcols] <- c("PCR1_index", "PCR2_index", "PCR3_index") # rename columns to "index"
   return(df)
