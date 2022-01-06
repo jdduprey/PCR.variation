5b051ac3b5d7036e9ddf31e0810270a144208c7a
jdduprey <joe.d.duprey@gmail.com>
2022-01-06 15:27:06 -0800

    wide and long form calcofi data

--- special_issue_manuscript/code/calcofi_data_wrangle.R
+++ special_issue_manuscript/code/calcofi_data_wrangle.R
@@ -13,18 +13,26 @@ library(ggplot2)
 micro_counts <- readRDS(here("special_issue_manuscript", "data", "microscopy_tech_nReads.RDS"))
 mifish_reads <- readRDS(here("special_issue_manuscript", "data", "mifish_tech_nReads.RDS"))
 
-counts_plus_reads <- left_join(micro_counts, mifish_reads)
+counts_plus_reads <- left_join(mifish_reads, micro_counts)
+
+print(unique(counts_plus_reads$tech_rep))
 
 # lets get the data into wide form 
 counts_plus_reads <- counts_plus_reads %>%
   mutate(bio_rep = paste(station_id, ext_rep, sep = "_")) %>%
-  select(-c(Sample,ID_sebastes)) %>%
-  pivot_wider(names_from = tech_rep, values_from = mifish_reads) %>%
-  distinct(ID_microscopy)
+  select(-c(Sample, ID_sebastes, Unique_ID)) %>%
+  distinct() 
 
-ggplot(counts_plus_reads, aes(x=mifish_reads)) + 
-  geom_histogram()
+write_csv(counts_plus_reads, here("special_issue_manuscript", "data", "calcofi_reads_counts_long.csv"))
 
-ggplot(counts_plus_reads, aes(x=larval_counts)) + 
-  geom_histogram()
+counts_plus_reads_wide <- counts_plus_reads %>%
+  pivot_wider(names_from = tech_rep, values_from = mifish_reads) 
+  
+write_csv(counts_plus_reads_wide, here("special_issue_manuscript", "data", "calcofi_reads_counts_wide.csv"))
+
+# ggplot(counts_plus_reads, aes(x=mifish_reads)) + 
+#   geom_histogram()
+# 
+# ggplot(counts_plus_reads, aes(x=larval_counts)) + 
+#   geom_histogram()
 
